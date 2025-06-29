4f187d81640ff7a9707b558cfadd770f5a77a538
Will Fitch <fitch.will@gmail.com>
2025-06-29 05:15:38 +0000

    added tiktoken_cache

--- vector_store.py
+++ vector_store.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import os
 
+tiktoken_cache_dir = "tiktoken_cache"
+os.environ["TIKTOKEN_CACHE_DIR"] = tiktoken_cache_dir
+
+# validate
+assert os.path.exists(os.path.join(tiktoken_cache_dir,"9b5ad71b2ce5302211f9c61530b329a4922fc6a4"))
+
+
 from snowflake.snowpark.types import StructType, StructField, StringType, VectorType, ArrayType, IntegerType
 
 from langchain_openai import OpenAIEmbeddings
